0a295f56821c5b3a4cb153aa3f8872159fa267af
mman9our <hack_225@outlook.sa>
2023-04-12 00:28:22 +0300

    update the code and sperate the task to three functions

--- main.js
+++ main.js
@@ -1,19 +1,62 @@
 const fs = require("fs");
 const csv = require("csv-parser");
 
-const users = [];
+function readCSV(filepath, onData) {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(filepath)
+            .pipe(csv({}))
+            .on("data", onData)
+            .on("end", () => {
+                console.log(`CSV file ${filepath} read successfully`);
+                resolve();
+            })
+            .on("error", (err) => {
+                reject(`Error reading CSV file ${filepath}: ${err}`);
+            });
+    });
+}
 
-fs.createReadStream("users.csv")
-    .pipe(csv({}))
-    .on("data", (data) => {
-        // Convert each line of data to a JS object and push it to the users array
-        users.push(data);
-    })
-    .on("end", () => {
-        // Write the users array as a JSON file
-        fs.writeFile("users.json", JSON.stringify(users), (err) => {
-            if (err) throw err;
-            else
-                console.log("Users data saved to users.json file");
+function saveToFile(data, filepath) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filepath, JSON.stringify(data), (err) => {
+            if (err) {
+                reject(`Error saving to file ${filepath}: ${err}`);
+            } else {
+                console.log(`Data saved to file ${filepath}`);
+                resolve();
+            }
         });
     });
+}
+
+function readJsonFile(filepath, onRead) {
+    fs.readFile(filepath, (err, data) => {
+        if (err) {
+            console.error(`Error reading from file ${filepath}:`, err);
+        } else {
+            try {
+                const jsonData = JSON.parse(data);
+                onRead(jsonData);
+            } catch (e) {
+                console.error(`Error parsing JSON from file ${filepath}:`, e);
+            }
+        }
+    });
+}
+
+// Example usage:
+async function main() {
+    const users = [];
+
+    await readCSV("users.csv", (data) => {
+        users.push(data);
+    });
+
+    await saveToFile(users, "users.json");
+
+    readJsonFile("users.json", (data) => {
+        console.log(`Read ${data.length} users from JSON file`);
+    });
+}
+
+main();
